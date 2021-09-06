2139c9cd084f44dcd64802d4319578839aed2d36
Eric Esteves Martins <estevesmartinseric@gmail.com>
2021-09-06 13:59:22 -0300

    css X

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,16 @@
 function TodoList({ list, handleTodo }) {
+  const styleX = {
+    color: "red",
+    fontWeight: 700,
+  };
   return (
     <ul>
       {list.map((element, index) => (
         <li key={index}>
           {element}
-          <button onClick={() => handleTodo(element)}>X</button>
+          <button style={styleX} onClick={() => handleTodo(element)}>
+            X
+          </button>
         </li>
       ))}
     </ul>
